61d81a8d209fc37e0a17a2d26f4da2cd7b1fe409
adriano.souza <adriano.souza@zpesystems.com>
2022-02-23 16:42:42 -0300

    Implementing properties to Tweet component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { Tweet } from "./components/Tweet"
 
 function App() {
-  return <Tweet />
+  return (
+    <>
+      <Tweet text="Tweet 1"/>
+      <Tweet text="Tweet 2"/>
+      <Tweet text="Tweet 3"/>
+    </>
+  );
 }
 
 export default App
