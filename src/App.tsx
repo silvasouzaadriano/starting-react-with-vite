6eaaf3ec3379548ad15d9b5d2f05db30f6337fa8
adriano.souza <adriano.souza@zpesystems.com>
2022-02-23 18:33:31 -0300

    Implementing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,10 @@
-import { useState } from "react";
-import { Tweet } from "./components/Tweet";
 import './App.css';
+import { AppRoutes } from './Routes'
 
 function App() {
-  const [tweets, setTweets] = useState<string[]>([
-    'Tweet 1',
-    'Tweet 2',
-    'Tweet 3',
-    'Tweet 4',
-  ])
-
-  function createTweet() {
-    let tweet = 'Tweet ' + (tweets.length + 1)
-  
-    setTweets([...tweets, tweet])
-  }
-    
-  return (
-    <>
-      {
-        tweets.map(tweet => {
-          return <Tweet text={tweet} />
-        })
-      }
-      <button
-       onClick={createTweet}
-       style={{
-         backgroundColor: '#8257e6',
-         border: 0,
-         padding: '6px 12px',
-         color: '#FFF'
-       }}
-      >
-        Add tweet
-      </button>
-    </>
-  );
+    return (
+      <AppRoutes />
+    );
 }
 
 export default App
